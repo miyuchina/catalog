09b85c73e09be25b448bca7d28499f2312584270
Mi Yu <miyu.china@icloud.com>
2018-10-27 21:06:02 -0400

    added schema

---
+++ lib/load.py
@@ -0,0 +1,60 @@
+import json
+import sqlite3
+
+def main():
+    with open('catalog.json', 'r') as fin:
+        courses = json.load(fin)
+
+    db = sqlite3.connect('app.sqlite')
+    cursor = db.cursor()
+    with open('lib/schema.sql', 'r') as fin:
+        cursor.executescript(fin.read())
+    db.commit()
+
+    for course in courses:
+        cursor.execute(
+            '''
+            INSERT INTO course
+                (dept, title, code, desc, deptnote, distnote, divattr,
+                 dreqs, enrollmentpref, expected, limit_, matlfee, prerequisites,
+                 rqmtseval, type)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            ''',
+            (course['dept'],
+             course['title'],
+             course['code'],
+             course['desc'],
+             unlist(course['deptnote']),
+             unlist(course['distnote']),
+             unlist(course['divattr']),
+             unlist(course['dreqs']),
+             unlist(course['enrollmentpref']),
+             course['expected'],
+             course['limit'],
+             unlist(course['matlfee']),
+             unlist(course['prerequisites']),
+             unlist(course['rqmtseval']),
+             course['type']
+            )
+        )
+        course_id = cursor.lastrowid
+        for section in course['sections']:
+            cursor.execute(
+                """
+                INSERT INTO section
+                    (instr, tp, type, course_id)
+                VALUES (?, ?, ?, ?)
+                """,
+                (unlist(section['instr']),
+                 unlist(section['tp']),
+                 section['type'],
+                 course_id)
+            )
+    db.commit()
+
+
+def unlist(l):
+    return ';;'.join(l)
+
+if __name__ == '__main__':
+    main()
---
+++ lib/schema.sql
@@ -0,0 +1,44 @@
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS bucket;
+DROP TABLE IF EXISTS course;
+DROP TABLE IF EXISTS section;
+
+CREATE TABLE user (
+    id       INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE bucket (
+    id      INTEGER PRIMARY KEY AUTOINCREMENT,
+    name    TEXT UNIQUE NOT NULL,
+    courses TEXT,
+    user_id INTEGER NOT NULL
+);
+
+CREATE TABLE course (
+    id             INTEGER PRIMARY KEY AUTOINCREMENT,
+    dept           TEXT,
+    title          TEXT,
+    code           INTEGER,
+    desc           TEXT,
+    deptnote       TEXT,
+    distnote       TEXT,
+    divattr        TEXT,
+    dreqs          TEXT,
+    enrollmentpref TEXT,
+    expected       INTEGER,
+    limit_         INTEGER,
+    matlfee        TEXT,
+    prerequisites  TEXT,
+    rqmtseval      TEXT,
+    type           TEXT
+);
+
+CREATE TABLE section (
+    id    INTEGER PRIMARY KEY AUTOINCREMENT,
+    instr TEXT,
+    tp    TEXT,
+    type  TEXT,
+    course_id INTEGER NOT NULL
+);
